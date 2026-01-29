4feb3eae8f331a6d6401b4f72180417a1fd0731d
unknown <ms3071687@gmail.com>
2026-01-29 14:29:18 +0500

    -----

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try{
-        await mongoose.connect("mongodb+srv://ms3071687_db_user:jnvzO7MrUdBTljRr@06chmscluster.9meug3h.mongodb.net/06chmsdb");
-        console.log("MongoDB Connected Successfully");
-    }
-    catch(err){
-        console.log(err);
-    }
-    
-}
+  try {
+    await mongoose.connect(
+      "mongodb+srv://ms3071687_db_user:TTGbInNzwonFk0ya@hotel-management-system.09jkfcn.mongodb.net/06chmsdb",
+    );
+    console.log("MongoDB Connected Successfully");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = connectDB;
-
